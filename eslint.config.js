dcf0ecc08e177e6ad648a732ead5d9612bf2f0ff
tejon17584 <alejandro.tejada.ui@gmail.com>
2025-07-18 11:10:02 -0600

    Configures ESLint rules
    Updates ESLint configuration to disable or modify certain rules.
    This improves compatibility with the project's coding style and
    reduces unnecessary warnings or errors. It disables rules related
    to React scope, explicit function return types, promises,
    nullable type assertions and strict boolean expressions.

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,14 @@ module.exports = defineConfig([
   {
     rules: {
       'react/no-unescaped-entities': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      'react/jsx-uses-vars': 'error',
+      '@typescript-eslint/non-nullable-type-assertion-style': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
     },
   },
 ]);
