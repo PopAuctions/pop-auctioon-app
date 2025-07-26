1a07811e3659ae247d2280a6035764141de9dc12
tejon17584 <alejandro.tejada.ui@gmail.com>
2025-07-26 00:48:19 -0600

    Update tests

--- __tests__/app/(tabs)/two.test.tsx
+++ __tests__/app/(tabs)/two.test.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
-import renderer, { act } from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 import TabTwoScreen from '../../../app/(tabs)/two';
 
 describe('TabTwoScreen', () => {
   it('renders correctly', () => {
-    let tree;
-    act(() => {
-      tree = renderer.create(<TabTwoScreen />).toJSON();
-    });
-    expect(tree).toMatchSnapshot();
+    const { getByText } = render(<TabTwoScreen />);
+
+    // Verificar que el texto principal se renderiza
+    expect(getByText('Tab Two')).toBeTruthy();
+    expect(getByText('Press me')).toBeTruthy();
+  });
+
+  it('should match snapshot', () => {
+    const { toJSON } = render(<TabTwoScreen />);
+
+    // Mantener el snapshot test usando la librería recomendada
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should render without crashing', () => {
+    // Test básico de smoke test
+    expect(() => render(<TabTwoScreen />)).not.toThrow();
   });
 });
