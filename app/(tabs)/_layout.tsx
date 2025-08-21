8ae0a7ca8d71bc85b67ae756e06a5f32c0c9872a
tejon17584 <alejandro.tejada.ui@gmail.com>
2025-08-20 23:37:31 -0600

    fixeando logica

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -23,254 +23,110 @@ export default function TabLayout() {
         },
       }}
     >
-      {/* 🔒 USUARIO LOGGEADO CON SESIÓN (role: UserRoles.USER) */}
-      {/* HOME - SUBASTA - TIENDA ONLINE - ACCOUNT */}
-      {session && role === 'USER' && (
-        <>
-          <Tabs.Screen
-            name='index/home'
-            options={{
-              title: t('tabsNames.home'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='home'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='auctions'
-            options={{
-              title: t('tabsNames.auctions'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='gavel'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='store'
-            options={{
-              title: t('tabsNames.store'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='shopping-cart'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='account'
-            options={{
-              title: t('tabsNames.account'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='user'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          {/* Hide tabs not needed for USER */}
-          <Tabs.Screen
-            name='my-auctions'
-            options={{
-              href: null,
-            }}
-          />
-          <Tabs.Screen
-            name='auth'
-            options={{
-              href: null,
-            }}
-          />
-        </>
-      )}
+      {/* HOME tab - visible for all users */}
+      <Tabs.Screen
+        name='index/home'
+        options={{
+          title: t('tabsNames.home'),
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              name='home'
+              size={28}
+              style={{ marginBottom: -3 }}
+              color={color}
+            />
+          ),
+        }}
+      />
 
-      {/* 🎭 USUARIO CON ROL SUBASTADOR (role: UserRoles.AUCTIONEER) */}
-      {/* HOME - SUBASTA - TIENDA - MIS SUBASTAS - ACCOUNT */}
-      {session && role === 'AUCTIONEER' && (
-        <>
-          <Tabs.Screen
-            name='index/home'
-            options={{
-              title: t('tabsNames.home'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='home'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='auctions'
-            options={{
-              title: t('tabsNames.auctions'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='gavel'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='store'
-            options={{
-              title: t('tabsNames.store'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='shopping-cart'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='my-auctions'
-            options={{
-              title: t('tabsNames.myAuctions'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='list'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='account'
-            options={{
-              title: t('tabsNames.account'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='user'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          {/* Hide auth tab for AUCTIONEER */}
-          <Tabs.Screen
-            name='auth'
-            options={{
-              href: null,
-            }}
-          />
-        </>
-      )}
+      {/* AUCTIONS tab - visible for all users */}
+      <Tabs.Screen
+        name='auctions'
+        options={{
+          title: t('tabsNames.auctions'),
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              name='gavel'
+              size={28}
+              style={{ marginBottom: -3 }}
+              color={color}
+            />
+          ),
+        }}
+      />
 
-      {/* 🚪 USUARIO SIN SESIÓN (NO LOGGEADO) */}
-      {/* HOME - SUBASTAS - TIENDA - LOGIN */}
-      {!session && (
-        <>
-          <Tabs.Screen
-            name='index/home'
-            options={{
-              title: t('tabsNames.home'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='home'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='auctions'
-            options={{
-              title: t('tabsNames.auctions'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='gavel'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='store'
-            options={{
-              title: t('tabsNames.store'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='shopping-cart'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name='auth'
-            options={{
-              title: t('tabsNames.login'),
-              headerShown: false,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name='sign-in'
-                  size={28}
-                  style={{ marginBottom: -3 }}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          {/* Hide my-auctions and account for non-logged users */}
-          <Tabs.Screen
-            name='my-auctions'
-            options={{
-              href: null,
-            }}
-          />
-          <Tabs.Screen
-            name='account'
-            options={{
-              href: null,
-            }}
-          />
-        </>
-      )}
+      {/* STORE tab - visible for all users */}
+      <Tabs.Screen
+        name='store'
+        options={{
+          title: t('tabsNames.store'),
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              name='shopping-cart'
+              size={28}
+              style={{ marginBottom: -3 }}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      {/* MY AUCTIONS tab - only visible for AUCTIONEER */}
+      <Tabs.Screen
+        name='my-auctions'
+        options={{
+          title: t('tabsNames.myAuctions'),
+          headerShown: false,
+          href: session && role === 'AUCTIONEER' ? undefined : null,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              name='list'
+              size={28}
+              style={{ marginBottom: -3 }}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      {/* ACCOUNT tab - only visible for logged users */}
+      <Tabs.Screen
+        name='account'
+        options={{
+          title: t('tabsNames.account'),
+          headerShown: false,
+          href: session ? undefined : null,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              name='user'
+              size={28}
+              style={{ marginBottom: -3 }}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      {/* AUTH tab - only visible for non-logged users */}
+      <Tabs.Screen
+        name='auth'
+        options={{
+          title: t('tabsNames.login'),
+          headerShown: false,
+          href: !session ? undefined : null,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              name='sign-in'
+              size={28}
+              style={{ marginBottom: -3 }}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
