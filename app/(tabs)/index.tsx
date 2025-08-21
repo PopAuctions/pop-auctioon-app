9ad112f9fa4a15ac57d9d2719e4e4fd28afac75e
tejon17584 <alejandro.tejada.ui@gmail.com>
2025-08-20 21:12:49 -0600

    refactor a tailwind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,18 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { useTranslation } from '@/hooks/useTranslation';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function HomeScreen() {
+  const { t } = useTranslation();
 
-export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text className='font-poppins-bold text-xl'>Tab One POPPINS</Text>
-      <Text style={{ fontFamily: 'Poppins_400Regular' }}>
-        Inter Black HELLO
+    <View className='flex-1 items-center justify-center bg-white px-4'>
+      <Text className='mb-2 text-center text-3xl font-bold text-gray-800'>
+        {t('screens.home.title')}
       </Text>
-
-      <View
-        style={styles.separator}
-        lightColor='#eee'
-        darkColor='rgba(255,255,255,0.1)'
-      />
-      <EditScreenInfo path='app/(tabs)/index.tsx' />
+      <Text className='text-center text-gray-600'>
+        {t('screens.home.subtitle')}
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
