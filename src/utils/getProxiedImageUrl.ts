018f5946544c110e17ca6d680332c43fa372bdc5
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-09-28 14:05:54 +0200

    new util function

---
+++ src/utils/getProxiedImageUrl.ts
@@ -0,0 +1,16 @@
+export function getProxiedImageUrl(
+  imageUrl: string,
+  appendDomain?: boolean
+): string {
+  // Supabase base path you want to strip
+  const basePath = '/storage/v1/object/public/develop/images/';
+
+  // Find the image filename
+  const index = imageUrl.indexOf(basePath);
+  if (index === -1) return imageUrl; // fallback to original if unexpected format
+
+  const filename = imageUrl.slice(index + basePath.length);
+  return appendDomain
+    ? `${process.env.EXPO_PUBLIC_BASE_URL}/images/${filename}`
+    : `/images/${filename}`;
+}
