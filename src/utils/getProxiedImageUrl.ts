7e15add0c8378d06779cdb9385f3a168a9003315
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-09-28 15:54:38 +0200

    remove unnecessary param

--- src/utils/getProxiedImageUrl.ts
+++ src/utils/getProxiedImageUrl.ts
@@ -1,7 +1,4 @@
-export function getProxiedImageUrl(
-  imageUrl: string,
-  appendDomain?: boolean
-): string {
+export function getProxiedImageUrl(imageUrl: string): string {
   // Supabase base path you want to strip
   const basePath = '/storage/v1/object/public/develop/images/';
 
@@ -10,7 +7,6 @@ export function getProxiedImageUrl(
   if (index === -1) return imageUrl; // fallback to original if unexpected format
 
   const filename = imageUrl.slice(index + basePath.length);
-  return appendDomain
-    ? `${process.env.EXPO_PUBLIC_BASE_URL}/images/${filename}`
-    : `/images/${filename}`;
+
+  return `${process.env.EXPO_PUBLIC_BASE_URL}/images/${filename}`;
 }
