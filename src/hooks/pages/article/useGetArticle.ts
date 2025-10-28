c39e57be880de912ee587c45db50e88ff3361e2e
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-10-28 11:32:42 +0100

    create hook to get article

---
+++ src/hooks/pages/article/useGetArticle.ts
@@ -0,0 +1,91 @@
+import { AuctionStatus } from '@/constants/auctions';
+import { useSecureApi } from '@/hooks/api/useSecureApi';
+import {
+  ActionResponse,
+  Article,
+  AuctionCategories,
+  LangMap,
+  RequestStatus,
+} from '@/types/types';
+import { useCallback, useEffect, useState } from 'react';
+
+interface ArticleWithAuction extends Article {
+  Auction: {
+    id: number;
+    startDate: string;
+    status: AuctionStatus;
+    title: string;
+    category: AuctionCategories;
+  };
+}
+
+export const useGetArticle = ({
+  articleId,
+  validateAuctionStatus = false,
+  getStatus = false,
+  publishedArticle = false,
+  getAuctionData = false,
+}: {
+  articleId: number;
+  validateAuctionStatus?: boolean;
+  getStatus?: boolean;
+  publishedArticle?: boolean;
+  getAuctionData?: boolean;
+}): ActionResponse<ArticleWithAuction | null> => {
+  const [article, setArticle] = useState<ArticleWithAuction | null>(null);
+  const [status, setStatus] = useState<RequestStatus>('idle');
+  const [errorMessage, setErrorMessage] = useState<LangMap | null>(null);
+  const { protectedGet } = useSecureApi();
+
+  const fetchArticle = useCallback(async () => {
+    setStatus('loading');
+    const params = new URLSearchParams();
+
+    if (validateAuctionStatus)
+      params.append('validateAuctionStatus', String(validateAuctionStatus));
+    if (getStatus) params.append('getStatus', String(getStatus));
+    if (publishedArticle)
+      params.append('publishedArticle', String(publishedArticle));
+    if (getAuctionData) params.append('getAuctionData', String(getAuctionData));
+
+    const res = await protectedGet<ArticleWithAuction>(
+      `/articles/${articleId}?${params}`
+    );
+
+    if (res.error) {
+      setStatus('error');
+      setErrorMessage({
+        en: 'Error fetching article',
+        es: 'Error al obtener el artículo',
+      });
+      return {
+        message: {
+          en: 'Error fetching auction',
+          es: 'Error al obtener la subasta',
+        },
+      };
+    }
+
+    setArticle(res.data || null);
+    setStatus('success');
+
+    return {
+      error: null,
+      success: null,
+      res,
+    };
+  }, [
+    articleId,
+    getAuctionData,
+    getStatus,
+    protectedGet,
+    publishedArticle,
+    validateAuctionStatus,
+  ]);
+
+  useEffect(() => {
+    fetchArticle();
+  }, [articleId, fetchArticle]);
+
+  return { data: article, status, errorMessage, setErrorMessage };
+};
