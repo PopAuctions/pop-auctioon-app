7a9c88bdd7f71b68d83d58c314440bc7ce9bfa75
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-11-26 16:41:54 +0100

    Display my old auctions in paginated page

--- app/(tabs)/my-auctions/old.tsx
+++ app/(tabs)/my-auctions/old.tsx
@@ -10,6 +10,7 @@ import { REQUEST_STATUS } from '@/constants';
 import { Loading } from '@/components/ui/Loading';
 import { CustomError } from '@/components/ui/CustomError';
 import { useGetMyOldAuctions } from '@/hooks/pages/my-auctions/useGetMyOldAuctions';
+import { Pagination } from '@/components/ui/Pagination';
 
 export default function MyOldAuctionsScreen() {
   const { locale, t } = useTranslation();
@@ -18,6 +19,9 @@ export default function MyOldAuctionsScreen() {
     status,
     errorMessage,
     refetch,
+    page,
+    setPage,
+    totalPages,
   } = useGetMyOldAuctions();
   const [refreshing, setRefreshing] = useState(false);
   const myAuctions = t('screens.myAuctions');
@@ -56,66 +60,64 @@ export default function MyOldAuctionsScreen() {
           />
         }
       >
-        <View className='mx-auto w-full max-w-5xl px-6 py-10'>
-          {/* Title */}
-          <CustomText
-            type='h1'
-            className='text-center'
-          >
-            {myAuctions.oldAuctionsTitle}
-          </CustomText>
-
-          {/* Auctions list */}
-          <View className='mt-6 w-full'>
-            <View className='flex w-full flex-col gap-5'>
-              {auctions.map((auction) => (
-                <View
-                  key={auction.id}
-                  className='w-full'
-                >
-                  <CustomLink
-                    href={`/my-auction/${auction.id}`}
-                    className='flex w-full flex-row gap-5'
-                  >
-                    {/* Image */}
-                    <View className='aspect-[3/4] w-full max-w-40 overflow-hidden rounded-lg'>
-                      <CustomImage
-                        src={auction.image}
-                        alt={auction.title}
-                        className='h-full w-full'
-                        resizeMode='cover'
-                      />
-                    </View>
-
-                    {/* Info */}
-                    <View className='flex flex-1 flex-col items-start justify-center'>
-                      <AuctionDisplayDateTime
-                        locale={locale}
-                        startDate={auction.startDate}
-                        singleLine={true}
-                        displayTime={false}
-                      />
-                      <CustomText
-                        type='h4'
-                        className='text-start'
-                      >
-                        {auction.title}
-                      </CustomText>
-                    </View>
-                  </CustomLink>
-                </View>
-              ))}
-            </View>
-
-            {auctions.length === 0 && (
-              <CustomText
-                type='h2'
-                className='mt-6 text-center text-cinnabar'
+        {/* Payment cards list */}
+        <View className='mt-4 flex flex-col items-center gap-4'>
+          <View className='flex w-full flex-col gap-5'>
+            {auctions.map((auction) => (
+              <View
+                key={auction.id}
+                className='w-full'
               >
-                {myAuctions.noAuctionsFound}
-              </CustomText>
-            )}
+                <CustomLink
+                  href={`/my-auction/${auction.id}`}
+                  className='flex w-full flex-row gap-5'
+                >
+                  {/* Image */}
+                  <View className='aspect-[3/4] w-full max-w-40 overflow-hidden rounded-lg'>
+                    <CustomImage
+                      src={auction.image}
+                      alt={auction.title}
+                      className='h-full w-full'
+                      resizeMode='cover'
+                    />
+                  </View>
+
+                  {/* Info */}
+                  <View className='flex flex-1 flex-col items-start justify-center'>
+                    <AuctionDisplayDateTime
+                      locale={locale}
+                      startDate={auction.startDate}
+                      singleLine={true}
+                      displayTime={false}
+                    />
+                    <CustomText
+                      type='h4'
+                      className='text-start'
+                    >
+                      {auction.title}
+                    </CustomText>
+                  </View>
+                </CustomLink>
+              </View>
+            ))}
           </View>
+
+          {auctions.length === 0 && (
+            <CustomText
+              type='h2'
+              className='mt-6 text-center text-cinnabar'
+            >
+              {myAuctions.noAuctionsFound}
+            </CustomText>
+          )}
+
+          {auctions.length > 0 && (
+            <Pagination
+              page={page}
+              totalPages={totalPages}
+              onPageChange={setPage}
+            />
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
