4ebc60b742340ef7ef485d331ff8cb1c0164cf61
tejon17584 <alejandro.tejada.ui@gmail.com>
2025-12-25 13:08:32 -0600

    Agregando dropdown para lenguages

--- src/components/info/ContactUsContent.tsx
+++ src/components/info/ContactUsContent.tsx
@@ -14,6 +14,7 @@ import {
   type ContactUsSchemaType,
 } from '@/utils/schemas/contact-us-schema';
 import { Checkbox } from 'expo-checkbox';
+import { Lang } from '@/types/types';
 
 // Helper function to safely parse bilingual error messages
 const parseErrorMessage = (
